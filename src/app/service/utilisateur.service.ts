db59962fc377cd81eb9cb8e793463fb049e908c7
Arrivant <104974854+Arrivant93@users.noreply.github.com>
2024-04-24 15:39:58 +0200

    correctif auth

--- src/app/service/utilisateur.service.ts
+++ src/app/service/utilisateur.service.ts
@@ -4,23 +4,30 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilisateurService {
-  url = "http://localhost:3000/utilisateurs";
-  url2 = "http://localhost:3000/auth/";
+  url = 'http://localhost:3000/utilisateurs';
+  url2 = 'http://localhost:3000/auth/';
   httpOptions = {
     headers: new HttpHeaders({
       'Access-Control-Allow-Origin': '*',
-    })
+      'Content-Type': 'application/json',
+    }),
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   addUtilisateur(utilisateur: Utilisateur): Observable<Utilisateur> {
     return this.http.post<any>(this.url, utilisateur, this.httpOptions);
   }
-  connection(mail: string, mdp: string): Observable<any> {
-    return this.http.post(this.url2 + "login", { email: mail, mdp: mdp }, this.httpOptions);
+  connection(username: string, password: string): Observable<any> {
+    console.log(username, password);
+
+    return this.http.post<any>(this.url2 + 'login', {
+      username: username,
+      password: password
+      ,
+    });
   }
 }
